461d6b35292705ad12588eec10e43d7282341734
yorapalt <yoraplat@student.arteveldehs.be>
2017-05-21 20:28:34 +0200

    commit

--- js/animate.js
+++ js/animate.js
@@ -21,4 +21,19 @@ $(document).ready(function () {
         }
     }
 
+    footerAlign();
+
 });
+
+$(window).resize(function () {
+    footerAlign();
+});
+
+
+function footerAlign() {
+    $('footer').css('display', 'block');
+    $('footer').css('height', 'auto');
+    var footerHeight = $('footer').outerHeight();
+    $('body').css('padding-bottom', footerHeight);
+    $('footer').css('height', footerHeight);
+}
