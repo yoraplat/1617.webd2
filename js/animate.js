b369ffbfcffd6877d4a26a9def3da8e93e5c83de
yorapalt <yoraplat@student.arteveldehs.be>
2017-05-24 16:37:19 +0200

    commit

--- js/animate.js
+++ js/animate.js
@@ -1,29 +1,4 @@
-$(document).ready(function () {
-    var menu = $(".column-5");
-    var menu_button = $(".hamburger_container");
-    var count = 0;
-    menu_button.click(slideDown);
 
-    function slideDown() {
-
-        if (count % 2 < 1) {
-            console.log("test");
-            $(".hamburger").attr("src", "../img/kruis.png");
-            menu.slideDown("slow", function () {
-                count++;
-            });
-
-        } else {
-            $(".hamburger").attr("src", "../img/hamburger.png");
-            menu.slideUp("slow", function () {
-                count++;
-            });
-        }
-    }
-
-    footerAlign();
-
-});
 
 $(window).resize(function () {
     footerAlign();
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("onload", autoHeight);
 
 function autoHeight(){
@@ -12,5 +13,31 @@ function autoHeight(){
 
 //autoHeight();
         
+*/
 
-                       
+var button = document.querySelector(".hamburger");
+var nav = document.querySelectorAll(".column-5");
+var count = 0;
+
+button.addEventListener("click", openAndClose);
+
+function openAndClose() {
+
+    if (count % 2 < 1) {
+        button.src = "../img/kruis.png";
+
+        for (i = 0; i < nav.length; ++i) {
+            nav[i].style.display = "block";
+        }
+        count++;
+
+    } else {
+        button.src = "../img/hamburger.png";
+
+        for (i = 0; i < nav.length; ++i) {
+            nav[i].style.display = "none";
+        }
+        count++;
+
+    }
+}
